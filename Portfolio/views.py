c69c303df48c9a7dcc1bf99c95c67f79cf2e5919
alionajanusko <alionajanusko@gmail.com>
2024-07-02 12:25:01 +0100

    Fix Portfolio app setup and update URLs

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Portfolio/index.html')
