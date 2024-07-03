d08bbfe59af5754f872a6ce3a7b76f003132c0fd
alionajanusko <alionajanusko@gmail.com>
2024-07-03 11:10:24 +0100

    Create files for about_me,projects,contact_me page

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_me/', views.about_me, name='about_me'),
+    path('projects/', views.about_me, name='projects'),
+    path('contact_me/', views.about_me, name='contact_me'),
     # Add other URL patterns as needed
 ]
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -7,3 +7,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Portfolio/index.html')
+def about_me(request):
+    return render(request, 'Portfolio/about_me.html')
+def projects(request):
+    return render(request, 'Portfolio/projects.html')
+
+def contact_me(request):
+    return render(request, 'Portfolio/contact_me.html')
