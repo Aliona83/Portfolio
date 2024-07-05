f79330ed4d8916a2e01d52d98cfa9f83d66683dd
alionajanusko <alionajanusko@gmail.com>
2024-07-05 15:40:57 +0100

    Create superuser,add resume link

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.main, name='main'),
-    # path('home/', views.home, name='home'),
     path('about_me/', views.about_me, name='about_me'),
     path('projects/', views.projects, name='projects'),
     path('contact_me/', views.contact_me, name='contact_me'),
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
+def base(request):
+    return render(request, 'Portfolio/base.html')
+
 def main(request):
     return render(request, 'Portfolio/main.html')
 
